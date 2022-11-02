5406ce569ada3c502d6fe6256873a3d319ad049b
Calvin Moser <cmoser@thesoftwareguild.com>
2022-11-02 15:45:01 -0400

    Add WindowMaker lecture

---
+++ WindowMaster/src/main/java/org/sg/windowmaster/WindowMaster.java
@@ -0,0 +1,99 @@
+package org.sg.windowmaster;
+
+import java.util.Scanner;
+
+public class WindowMaster {
+
+
+    // { - start of a code block
+
+    public static void main(String[] args) {
+
+        // int i = 2;
+//        int j = 7;
+        // for -- know ahead of block how many times (iterator)
+        // while -- vague condition
+
+        for (int i = 5; i < 10;  i += 2) {
+            System.out.println(i);
+            // Get input from user
+            //i = i + 1;
+            //i += 2;
+            //i++;
+            //i = 2;
+        }
+
+        /*
+        if (i > 1) {
+            int j = 0;
+            i = i + 1;
+        } else {
+            i = i - 1;
+        }
+         */
+
+
+        //System.out.println("i: " + i);
+
+    }
+//
+//    // Write cool stuffs
+//    // Comment
+//    // <ctrl> /
+//
+//    /*
+//        You can stuff that is
+//        multiline
+//
+//
+//     */
+//
+//    // Identifiers
+//    // Java keywords and method/variable/class names
+//    // package, class, final, static
+//
+//
+//        // Variables
+//
+//        // Data type + identifier
+//        // Variable declaration
+//        int a = 3;
+//        String b = "bad variable names";
+//        boolean c = false;
+//
+//        // literal
+//        // initialization
+//
+//        // Expression
+//
+//        int d = 1 + 2;  // this has everything!
+//
+//
+//        // Declare variables
+//        float height;
+//        float width;
+//
+//        String stringHeight;
+//        String stringWidth;
+//
+//        // TODO: add other variables
+//
+//        // Get input
+//        // Instantiate
+//        Scanner myScanner = new Scanner(System.in);
+//
+//        System.out.println("Please enter window height:");
+//        // stringHeight = myScanner.nextLine();
+//        height = myScanner.nextFloat();
+//
+////        height = Float.parseFloat(stringHeight);
+//
+//        System.out.println(height);
+//
+//        // Make calculatinos
+//
+//        // Give information back to user
+//
+//    }
+
+}
