380e76d1f60a872d4776095e402f36eb8542e52d
Calvin Moser <cmoser@thesoftwareguild.com>
2022-11-02 15:50:37 -0400

    Finish out procedural version

--- WindowMaster/src/main/java/org/sg/windowmaster/WindowMaster.java
+++ WindowMaster/src/main/java/org/sg/windowmaster/WindowMaster.java
@@ -4,96 +4,44 @@ import java.util.Scanner;
 
 public class WindowMaster {
 
-
-    // { - start of a code block
-
     public static void main(String[] args) {
 
-        // int i = 2;
-//        int j = 7;
-        // for -- know ahead of block how many times (iterator)
-        // while -- vague condition
+        // Declare variables
+        float height;
+        float width;
 
-        for (int i = 5; i < 10;  i += 2) {
-            System.out.println(i);
-            // Get input from user
-            //i = i + 1;
-            //i += 2;
-            //i++;
-            //i = 2;
-        }
+        String stringHeight;
+        String stringWidth;
 
-        /*
-        if (i > 1) {
-            int j = 0;
-            i = i + 1;
-        } else {
-            i = i - 1;
-        }
-         */
+        float areaOfWindow;
+        float cost;
+        float perimeterOfWindow;
 
+        // Get input
+        // Instantiate
+        Scanner myScanner = new Scanner(System.in);
 
-        //System.out.println("i: " + i);
+        System.out.println("Please enter window height:");
+        stringHeight = myScanner.nextLine();
+        System.out.println("Please enter window width:");
+        stringWidth = myScanner.nextLine();
+
+        // Make calculatinos
+        height = Float.parseFloat(stringHeight);
+        width = Float.parseFloat(stringWidth);
+
+        areaOfWindow = height * width;
+        perimeterOfWindow = 2 * (height + width);
+        cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
+
+        // Give information back to user
+        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window area = " + areaOfWindow);
+        System.out.println("Window perimeter = " + perimeterOfWindow);
+        System.out.println("Total Cost =  " + cost);
+
 
     }
-//
-//    // Write cool stuffs
-//    // Comment
-//    // <ctrl> /
-//
-//    /*
-//        You can stuff that is
-//        multiline
-//
-//
-//     */
-//
-//    // Identifiers
-//    // Java keywords and method/variable/class names
-//    // package, class, final, static
-//
-//
-//        // Variables
-//
-//        // Data type + identifier
-//        // Variable declaration
-//        int a = 3;
-//        String b = "bad variable names";
-//        boolean c = false;
-//
-//        // literal
-//        // initialization
-//
-//        // Expression
-//
-//        int d = 1 + 2;  // this has everything!
-//
-//
-//        // Declare variables
-//        float height;
-//        float width;
-//
-//        String stringHeight;
-//        String stringWidth;
-//
-//        // TODO: add other variables
-//
-//        // Get input
-//        // Instantiate
-//        Scanner myScanner = new Scanner(System.in);
-//
-//        System.out.println("Please enter window height:");
-//        // stringHeight = myScanner.nextLine();
-//        height = myScanner.nextFloat();
-//
-////        height = Float.parseFloat(stringHeight);
-//
-//        System.out.println(height);
-//
-//        // Make calculatinos
-//
-//        // Give information back to user
-//
-//    }
 
 }
