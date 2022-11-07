c742a30b8d202f1ebdb558b13d832d33bff9a44a
Calvin Moser <cmoser@thesoftwareguild.com>
2022-11-07 09:51:04 -0500

    Add refactor lecture code

--- WindowMaster/src/main/java/org/sg/windowmaster/WindowMaster.java
+++ WindowMaster/src/main/java/org/sg/windowmaster/WindowMaster.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class WindowMaster {
 
+    private static Scanner myScanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
 
         // Declare variables
@@ -22,35 +25,57 @@ public class WindowMaster {
 
         // Get input
         // Instantiate
-        Scanner myScanner = new Scanner(System.in);
+        //Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Please enter the family name:");
-        familyName = myScanner.nextLine();
+        //System.out.println("Please enter the family name:");
+        //familyName = myScanner.nextLine();
+        familyName = retrieveFamilyName();
         System.out.println("Please enter the given name:");
         givenName = myScanner.nextLine();
-        System.out.println("Please enter window height:");
-        stringHeight = myScanner.nextLine();
+//        System.out.println("Please enter window height:");
+//        stringHeight = myScanner.nextLine();
         System.out.println("Please enter window width:");
         stringWidth = myScanner.nextLine();
 
         // Make calculatinos
-        height = Float.parseFloat(stringHeight);
+        //height = Float.parseFloat(stringHeight);
+        height = retrieveHeight();
         width = Float.parseFloat(stringWidth);
 
         areaOfWindow = height * width;
         perimeterOfWindow = 2 * (height + width);
         cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
 
+        double a = 3.5;
+        float b = 3.5f;
+        int c = 2;
+        long d = 22222222222l;
+
         // Give information back to user
         System.out.println("Customer family name: " + familyName);
         System.out.println("Customer given name: " + givenName);
-        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window height = " + height);
         System.out.println("Window width = " + stringWidth);
         System.out.println("Window area = " + areaOfWindow);
         System.out.println("Window perimeter = " + perimeterOfWindow);
-        System.out.println("Total Cost =  " + cost);
+        printCost(cost);
 
 
     }
 
+    private static String retrieveFamilyName() {
+        System.out.println("Please enter the family name:");
+        String name = myScanner.nextLine();
+        return name;
+    }
+
+    private static float retrieveHeight() {
+        System.out.println("Please enter window height:");
+        String stringHeight = myScanner.nextLine();
+        return Float.parseFloat(stringHeight);
+    }
+
+    private static void printCost(float cost) {
+        System.out.println("Total Cost =  " + cost);
+    }
 }
